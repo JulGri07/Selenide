ea40f6bea1a9d322bb3e3c2a66b42cbc13f5ff4b
Jul <yuliiahryshanova@gmail.com>
2022-06-06 17:11:41 +0300

    testNG with DataProvider

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -2,76 +2,95 @@ import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
 
+    private By first_name = By.xpath(".//*[@name='firstname']");
+    private By last_name = By.xpath(".//*[@name='lastname']");
+    private By reg_email = By.xpath(".//*[@name='reg_email__']");
+    private By reg_email_confirm = By.xpath(".//*[@name='reg_email_confirmation__']");
+    private By password = By.xpath(".//*[@id='password_step_input']");
+    private By birth_day = By.xpath(".//*[@id='day']");
+    private By birth_month = By.xpath(".//*[@id='month']");
+    private By birth_year = By.xpath(".//*[@id='year']");
+    private By sex = By.xpath(".//input[@name='sex']");
+
+    private By reg_button = By.xpath(".//*[@data-testid='open-registration-form-button']");
+    private By submit_button = By.xpath(".//button[@name='websubmit']");
+
+    @DataProvider(name = "inputData")
+    public FBClassUser[][] inputUser() {
+        return new FBClassUser[][] {{new FBClassUser(
+                "User",
+                "NewComer",
+                "set.email@gmail.com",
+                "QwertY123",
+                "27",
+                "6",
+                "1984",
+                0)}};
+    }
+
     @Test
     public void openRegForm() {
         Configuration.browserSize = "1920x1080";
         open("https://www.facebook.com/");
-        $(By.xpath(".//*[@data-testid='open-registration-form-button']")).should(Condition.enabled);
-        $(By.xpath(".//*[@data-testid='open-registration-form-button']")).click();
+        $(reg_button).should(Condition.enabled);
+        $(reg_button).click();
         $(".registration_redesign").should(Condition.appear);
     }
 
     @Test(dependsOnMethods = "openRegForm")
-    public void checkRegFormElements() throws InterruptedException {
-        $(By.xpath(".//*[@name='firstname']")).shouldBe(Condition.visible);
-        $(By.xpath(".//*[@name='lastname']")).shouldBe(Condition.visible);
-        $(By.xpath(".//*[@name='reg_email__']")).shouldBe(Condition.visible);
-        $(By.xpath(".//*[@name='reg_email_confirmation__']")).shouldBe(Condition.hidden);
-        $(By.xpath(".//*[@id='password_step_input']")).shouldBe(Condition.visible);
-        $(By.xpath(".//*[@id='day']")).shouldBe(Condition.visible);
-        $(By.xpath(".//*[@id='month']")).shouldBe(Condition.visible);
-        $(By.xpath(".//*[@id='year']")).shouldBe(Condition.visible);
-
-        $$(By.xpath(".//input[@name='sex']")).shouldHave(CollectionCondition.size(3));
-
+    public void checkRegFormElements()  {
+        $(first_name).shouldBe(Condition.visible);
+        $(last_name).shouldBe(Condition.visible);
+        $(reg_email).shouldBe(Condition.visible);
+        $(reg_email_confirm).shouldBe(Condition.hidden);
+        $(password).shouldBe(Condition.visible);
+        $(birth_day).shouldBe(Condition.visible);
+        $(birth_month).shouldBe(Condition.visible);
+        $(birth_year).shouldBe(Condition.visible);
+        $$(sex).shouldHave(CollectionCondition.size(3));
     }
 
-    @Test(dependsOnMethods = "checkRegFormElements")
-    public void fillRegistrationForm() throws InterruptedException {
-
-        $(By.xpath(".//*[@name='firstname']")).setValue("User");
-        $(By.xpath(".//*[@name='lastname']")).setValue("NewComer");
-        $(By.xpath(".//*[@name='reg_email__']")).setValue("set.email@gmail.com");
-        $(By.xpath(".//*[@id='password_step_input']")).setValue("Qwerty123");
-        $(By.xpath(".//*[@id='day']")).selectOptionByValue("27");
-        $(By.xpath(".//*[@id='month']")).selectOptionByValue("6");
-        $(By.xpath(".//*[@id='year']")).selectOptionByValue("1984");
-
-        $$(By.xpath(".//input[@name='sex']")).get(0).click();
-
-        Thread.sleep(1000);
+    @Test(dependsOnMethods = "checkRegFormElements", dataProvider = "inputData")
+    public void fillRegistrationForm(FBClassUser user) {
+        $(first_name).setValue(user.first_name);
+        $(last_name).setValue(user.last_name);
+        $(reg_email).setValue(user.reg_email);
+        $(password).setValue(user.password);
+        $(birth_day).selectOptionByValue(user.birth_day);
+        $(birth_month).selectOptionByValue(user.birth_month);
+        $(birth_year).selectOptionByValue(user.birth_year);
+        $$(sex).get(user.sex).click();
     }
 
-    @Test(dependsOnMethods = "fillRegistrationForm")
-    public void checkEmailConfirmation(){
-        $(By.xpath(".//*[@name='reg_email_confirmation__']")).shouldBe(Condition.visible);
-        $(By.xpath(".//*[@name='reg_email_confirmation__']")).setValue("set.email@gmail.com");
-        $(By.xpath(".//*[@name='reg_email_confirmation__']")).shouldHave(Condition.exactValue("set.email@gmail.com"));
+    @Test(dependsOnMethods = "fillRegistrationForm", dataProvider = "inputData")
+    public void checkEmailConfirmation(FBClassUser user) {
+        $(reg_email_confirm).shouldBe(Condition.visible);
+        $(reg_email_confirm).setValue(user.reg_email);
+        $(reg_email_confirm).shouldHave(Condition.exactValue(user.reg_email));
     }
 
-    @Test(dependsOnMethods = "fillRegistrationForm")
-    public void checkRegForm(){
-        $(By.xpath(".//*[@name='firstname']")).shouldHave(Condition.exactValue("User"));
-        $(By.xpath(".//*[@name='lastname']")).shouldHave(Condition.exactValue("NewComer"));
-        $(By.xpath(".//*[@name='reg_email__']")).shouldHave(Condition.exactValue("set.email@gmail.com"));
-        $(By.xpath(".//*[@id='password_step_input']")).shouldHave(Condition.exactValue("Qwerty123"));
-        $(By.xpath(".//*[@id='day']")).shouldBe(Condition.value("27"));
-        $(By.xpath(".//*[@id='month']")).shouldBe(Condition.value("6"));
-        $(By.xpath(".//*[@id='year']")).shouldBe(Condition.value("1984"));
-
-        $$(By.xpath(".//input[@name='sex']")).get(0).shouldBe(Condition.selected);
+    @Test(dependsOnMethods = "fillRegistrationForm", dataProvider = "inputData")
+    public void checkRegForm(FBClassUser user){
+        $(first_name).shouldHave(Condition.exactValue(user.first_name));
+        $(last_name).shouldHave(Condition.exactValue(user.last_name));
+        $(reg_email).shouldHave(Condition.exactValue(user.reg_email));
+        $(password).shouldHave(Condition.exactValue(user.password));
+        $(birth_day).shouldBe(Condition.value(user.birth_day));
+        $(birth_month).shouldBe(Condition.value(user.birth_month));
+        $(birth_year).shouldBe(Condition.value(user.birth_year));
+        $$(sex).get(user.sex).shouldBe(Condition.selected);
     }
 
     @Test(dependsOnMethods = "fillRegistrationForm")
     public void checkRegButton(){
-        $(By.xpath(".//button[@name='websubmit']")).shouldBe(Condition.enabled);
+        $(submit_button).shouldBe(Condition.enabled);
     }
-
 
 }
